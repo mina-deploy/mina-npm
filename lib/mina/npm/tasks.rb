668c47d7ec4091f7f6857b56037ecc134fcf0569
Anton Kandybo <dublicator@hotmail.com>
2017-01-22 14:42:08 +0200

    Added yarn support

--- lib/mina/npm/tasks.rb
+++ lib/mina/npm/tasks.rb
@@ -1,11 +1,12 @@
 set :npm_bin, 'npm'
 set :bower_bin, 'bower'
 set :grunt_bin, 'grunt'
+set :yarn_bin, 'yarn'
 set :npm_options, '--production'
 set :bower_options, '--allow-root'
 set :grunt_options, ''
 set :grunt_task, 'build'
-
+set :yarn_options, '--production'
 
 namespace :npm do
   desc 'Install node modules using Npm.'
@@ -16,6 +17,19 @@ namespace :npm do
       #{echo_cmd %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/$sub_directory/node_modules"]}
       #{echo_cmd %[ln -s "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/$sub_directory/node_modules" "node_modules"]}
       #{echo_cmd %[#{fetch(:npm_bin)} install #{fetch(:npm_options)}]}
+    }
+  end
+end
+
+namespace :yarn do
+  desc 'Install node modules using Yarn.'
+  task install: :environment do
+    command %{
+      echo "-----> Installing node modules using Yarn"
+      sub_directory=$(pwd | sed -r "s/.*?$(basename $build_path)//g")
+      #{echo_cmd %[mkdir -p "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/$sub_directory/node_modules"]}
+      #{echo_cmd %[ln -s "#{fetch(:deploy_to)}/#{fetch(:shared_path)}/$sub_directory/node_modules" "node_modules"]}
+      #{echo_cmd %[#{fetch(:yarn_bin)} install #{fetch(:yarn_options)}]}
     }
   end
 end
